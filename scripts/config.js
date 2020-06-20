28f2f594c68e827d5c0caf954dbee6c9412997c6
zwoodard <zane.kt.woodard@gmail.com>
2020-06-20 07:56:10 -1000

    Add form validation on submit

---
+++ scripts/config.js
@@ -0,0 +1,6 @@
+var config = {
+    max_donation_amount: 100000,
+    max_donation_message: "Thank you for your generosity. If you are planning to donate more than $100,000, please contact us first.",
+    min_donation_amount: 250,
+    min_donation_message: "Thank you for your generosity. However, at this time we only accept donations greater than $250.",
+};
